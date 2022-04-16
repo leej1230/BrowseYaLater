bf99751aefe94601f34fa2d7519067c0dd1d2b51
leej1230 <leejaewoo1230@gmail.com>
2022-04-16 15:01:52 -0700

    Adding logo and js working

--- js/popupmenu.js
+++ js/popupmenu.js
@@ -1,21 +1,27 @@
 const link_queue = [];
 
-const submit_button = document.querySelector('submit_button');
-const tab_button = document.querySelector('open_button');
-const link_button = document.querySelector('link_button');
+const submit_button = document.getElementById('submit_button');
+const tab_button = document.getElementById('open_button');
+const link_button = document.getElementById('link_button');
 
-submit_button.addEventListener('click', event => {
-  const web_link = document.getElementById("mytext");
-  //mytext is id of input for in html
-  link_queue.push(web_link.value);
+submit_button.addEventListener('click', () => {
+    const web_link = document.getElementById("mytext");
+    //mytext is id of input for in html
+    link_queue.push(web_link.value);
+    });
+
+tab_button.addEventListener('click', () => {
+var action_url = link_queue.shift();
+if(action_url == undefined){
+    var sampleArea = document.getElementById("sampleArea");
+    sampleArea.innerHTML = "You have went through everything!"
+}else{
+    chrome.tabs.create({ url: action_url });
+}
+
 });
 
-tab_button.addEventListener('click', event => {
-  var action_url = link_queue.pop;
-  chrome.tabs.create({ url: action_url });
-});
-
-link_button.addEventListener('click', event => {
-  var action_url = link_queue.pop;
-  location = action_url;
+link_button.addEventListener('click', () => {
+var action_url = link_queue.shift();
+location = action_url;
 });
