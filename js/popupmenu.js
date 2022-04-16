8afa155b183a61eb308e289c190b0eb2d3cb2025
leej1230 <leejaewoo1230@gmail.com>
2022-04-16 14:29:28 -0700

    all the events

--- js/popupmenu.js
+++ js/popupmenu.js
@@ -1,9 +1,21 @@
 const link_queue = [];
 
-const button = document.querySelector('button');
+const submit_button = document.querySelector('submit_button');
+const tab_button = document.querySelector('open_button');
+const link_button = document.querySelector('link_button');
 
-button.addEventListener('click', event => {
+submit_button.addEventListener('click', event => {
   const web_link = document.getElementById("mytext");
   //mytext is id of input for in html
   link_queue.push(web_link.value);
+});
+
+tab_button.addEventListener('click', event => {
+  var action_url = link_queue.pop;
+  chrome.tabs.create({ url: action_url });
+});
+
+link_button.addEventListener('click', event => {
+  var action_url = link_queue.pop;
+  location = action_url;
 });
