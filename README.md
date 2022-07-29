fa9351f55e0b966db6af996c420d563dd60c2f6a
leej1230 <leejaewoo1230@gmail.com>
2022-07-30 01:41:24 +0900

    Dexie.js, first commit. No test done yet

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
 This is a chrome's extention that supports people stay on focus by saving the distracting links for later on.
+
 Currently, I am working on couple issues to complete the project.
+
 Completion of project is pretty near. Stay Tuned!
 
 ### TODO List
 
-- [x] Test local storage chrome API and see how it works
-- [ ] Find the way to save link on local storage
-- [ ] Find the way to save string array on local storage
+- [x] Test local storage chrome API and see how it works (Update: used Indexed DB with Dexie.js)
+- [x] Find the way to save link on local storage
+- [x] Find the way to save string array on local storage
 - [ ] Find the way to change the inside of the array
 - [ ] Make sure the extention works properly
 - [ ] Go over HTML or CSS to see if the project projects properly on any devices
@@ -18,3 +20,6 @@ Completion of project is pretty near. Stay Tuned!
 - [ ] Allow users to delete the specific link they choose
 - [ ] Allow users to change the order of the link
 - [ ] Allow users to add the link to specific place
+
+### UPDATES
+- 7/30: Applied Dexie.js instead (Tried using chrome local storage api previously)
--- js/popupmenu.js
+++ js/popupmenu.js
@@ -1,6 +1,7 @@
 const submit_button = document.getElementById('submit_button');
 const tab_button = document.getElementById('open_button');
 const link_button = document.getElementById('link_button');
+var DBName = 'browseyalater';
 
 
 //Sending a Link to Queue
@@ -8,15 +9,24 @@ submit_button.addEventListener('click', () => {
     const web_link = document.getElementById("mytext"); //Get input link
     alert('Input is: ' + web_link.value);
     if(web_link.value){
-        chrome.storage.local.get(['number_of_tabs'], function(number) {
-            if(number.number_of_tabs == undefined){ //Initialize process
-                var tmp = 1;
-            } else {
-                var tmp = number.number_of_tabs+1;
-            }
-            chrome.storage.local.set({"number_of_tabs":tmp}, function(){});
-            alert('Value currently is ' + tmp);
+        var db = new Dexie(DBName);
+        // Data store name: URL, Key is auto increment, Data: URLAddress
+        db.version(1).stores({
+            URL: '++id, URLAddress'
+        });
+
+        db.URL.put({
+            URLAddress: web_link.value
         })
+        // chrome.storage.local.get(['number_of_tabs'], function(number) {
+        //     if(number.number_of_tabs == undefined){ //Initialize process
+        //         var tmp = 1;
+        //     } else {
+        //         var tmp = number.number_of_tabs+1;
+        //     }
+        //     chrome.storage.local.set({"number_of_tabs":tmp}, function(){});
+        //     alert('Value currently is ' + tmp);
+        // })
     }
 
     //Make the Input form back to Empty
@@ -25,36 +35,56 @@ submit_button.addEventListener('click', () => {
 
 //Open on NEW tab
 tab_button.addEventListener('click', () => {
-    chrome.storage.local.get(['number_of_tabs'], function(number) {
-        if(number.number_of_tabs == undefined){ //Initialize process
-            alert('Number of tabs were initialized, it is 0 and request denied')
-            number.number_of_tabs=0;
-        } else if(number.number_of_tabs == 0){ //NO Link
-            alert('There is no link to pull, request denied');
-        } else {
-            var tmp = number.number_of_tabs - 1;
-            alert('Link is opened, previously, there were ' + number.number_of_tabs + 'links, now the number of link is: ' + tmp);
+    Dexie.exists(DBName).then(function(exists){
+        if(exists){
+            var db = new Dexie(DBName);
+            if(db.URLAddress.get(0) != undefined){
+                var link_add = db.URLAddress.last();
+                chrome.tabs.create({ url: link_add });
+                db.URLAddress.last().delete();
+            }
         }
-        chrome.storage.local.set({"number_of_tabs":tmp}, function(){});
-        // To open link on NEW TAB
+    });
+    // chrome.storage.local.get(['number_of_tabs'], function(number) {
+    //     if(number.number_of_tabs == undefined){ //Initialize process
+    //         alert('Number of tabs were initialized, it is 0 and request denied')
+    //         number.number_of_tabs=0;
+    //     } else if(number.number_of_tabs == 0){ //NO Link
+    //         alert('There is no link to pull, request denied');
+    //     } else {
+    //         var tmp = number.number_of_tabs - 1;
+    //         alert('Link is opened, previously, there were ' + number.number_of_tabs + 'links, now the number of link is: ' + tmp);
+    //     }
+    //     chrome.storage.local.set({"number_of_tabs":tmp}, function(){});
+    // })    
+    // To open link on NEW TAB
         // chrome.tabs.create({ url: action_url });
-    })
 });
 
 //Open on CURRENT tab
 link_button.addEventListener('click', () => {
-    chrome.storage.local.get(['number_of_tabs'], function(number) {
-        if(number.number_of_tabs == undefined){ //Initialize process
-            alert('Number of tabs were initialized, it is 0 and request denied')
-            number.number_of_tabs=0;
-        } else if(number.number_of_tabs == 0){ //NO Link
-            alert('There is no link to pull, request denied');
-        } else {
-            var tmp = number.number_of_tabs - 1;
-            alert('Link is opened, previously, there were ' + number.number_of_tabs + 'links, now the number of link is: ' + tmp);
+    Dexie.exists(DBName).then(function(exists){
+        if(exists){
+            var db = new Dexie(DBName);
+            if(db.URLAddress.get(0) != undefined){
+                var link_add = db.URLAddress.last();
+                chrome.tabs.update({ url: link_add });
+                db.URLAddress.last().delete();
+            }
         }
-        chrome.storage.local.set({"number_of_tabs":tmp}, function(){});
-    })
+    });
+    // chrome.storage.local.get(['number_of_tabs'], function(number) {
+    //     if(number.number_of_tabs == undefined){ //Initialize process
+    //         alert('Number of tabs were initialized, it is 0 and request denied')
+    //         number.number_of_tabs=0;
+    //     } else if(number.number_of_tabs == 0){ //NO Link
+    //         alert('There is no link to pull, request denied');
+    //     } else {
+    //         var tmp = number.number_of_tabs - 1;
+    //         alert('Link is opened, previously, there were ' + number.number_of_tabs + 'links, now the number of link is: ' + tmp);
+    //     }
+    //     chrome.storage.local.set({"number_of_tabs":tmp}, function(){});
+    // })
     // To open link on CURRENT TAB
-    // chrome.tabs.create({ url: action_url });
+    // chrome.tabs.update({ url: action_url });
 });
